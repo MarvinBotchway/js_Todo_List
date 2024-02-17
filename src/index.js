2eb0a2781e5c86c2da9fc0c2f79fce075be68ab3
Marvin Botchway <marvinbotchway@outlook.com>
2024-02-17 11:30:31 +0000

    Add Todo class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import "./styles.css";
+import Todo from "./Todo.js";
 
-(function component() {
-  const content = document.getElementById("content");
-  const txt = document.createElement("p");
+let todo = new Todo(
+  "Cook",
+  "I want to practice my cooking skills",
+  new Date(2024, 18, 2),
+  "high"
+);
 
-  txt.textContent = "Hello World!";
-  content.appendChild(txt);
-})();
+const Content = document.getElementById("content");
+const Title = document.createElement("h3");
+const Description = document.createElement("h3");
+const TodoDate = document.createElement("h3");
+const priority = document.createElement("h3");
+
+Title.textContent = todo.title;
+Description.textContent = todo.description;
+
+Content.appendChild(Title);
+Content.appendChild(Description);
