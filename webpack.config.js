5cbb1bde453c09d3f591953112c5e1c4376bb63e
Marvin Botchway <marvinbotchway@outlook.com>
2024-02-17 10:53:57 +0000

    Add webpack-dev-server
    Also added scripts to build and start server.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
+  devServer: {
+    static: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
