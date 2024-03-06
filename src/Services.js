c52228bd755b5b4f0846f9616b0b05b30a140866
Marvin Botchway <marvinbotchway@outlook.com>
2024-03-06 19:29:19 +0000

    Show Todos For A Particular List When Clicked.

--- src/Services.js
+++ src/Services.js
@@ -1,8 +1,19 @@
 import Lists from "./data/Lists.js";
+import Todos from "./data/Todos.js";
 
 const addList = function (list) {
   Lists.push(list);
   return Lists;
 };
 
-export { addList };
+const getListTodos = function (list) {
+  let todos = [];
+  Todos.forEach((todo) => {
+    if (todo.list == list) {
+      todos.push(todo);
+    }
+  });
+  return todos;
+};
+
+export { addList, getListTodos };
--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -1,3 +1,6 @@
+import { clearPage, createPage } from "../Page";
+import { getListTodos } from "../Services";
+
 export default function ListCard(list) {
   const listCard = document.createElement("div");
   const title = document.createElement("h3");
@@ -6,5 +9,12 @@ export default function ListCard(list) {
   title.textContent = list.title;
 
   listCard.appendChild(title);
+
+  listCard.addEventListener("click", () => {
+    let todos = getListTodos(list);
+    clearPage();
+    createPage(todos, "todo");
+  });
+
   return listCard;
 }
