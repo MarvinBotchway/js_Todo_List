4c04a0fdcbf7fd8253bc38e5fc34ba3b4af6931d
Marvin Botchway <marvinbotchway@outlook.com>
2024-03-13 11:20:04 +0000

    Add Edit and View Todos Buttons To ListCards
    Hooked Up View Todos Button.

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -3,15 +3,24 @@ import { getListTodos } from "../Services";
 
 export default function ListCard(list) {
   const listCard = document.createElement("div");
+  const buttonsSection = document.createElement("div");
+  const editButton = document.createElement("button");
+  const todosButton = document.createElement("button");
   const title = document.createElement("h3");
+
+  editButton.textContent = "Edit";
+  todosButton.textContent = "View Todos";
 
   listCard.dataset.id = list.id;
   listCard.classList += "list-card card";
   title.textContent = list.title;
 
   listCard.appendChild(title);
+  buttonsSection.appendChild(editButton);
+  buttonsSection.appendChild(todosButton);
+  listCard.appendChild(buttonsSection);
 
-  listCard.addEventListener("click", () => {
+  todosButton.addEventListener("click", () => {
     let todos = getListTodos(list);
     clearPage();
     createPage(todos, "todo");
