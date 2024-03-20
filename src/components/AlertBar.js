683a07a1fd2ce535ec2643635c0e1d22527d4374
Marvin Botchway <marvinbotchway@outlook.com>
2024-03-20 18:23:41 +0000

    Fix Bug Which Allows Clicking Undo Multiple Times.
    Also updated styling of AlertBar and the message
    shown.

--- src/components/AlertBar.js
+++ src/components/AlertBar.js
@@ -13,31 +13,36 @@ export default function AlertBar(item, type) {
   const Message = document.createElement("h3");
   const UndoBtn = document.createElement("button");
 
+  let undoClicked = false;
+
   Bar.id = "alert";
-  Message.innerText = `${item.title} Deleted`;
+  Message.innerText = `"${item.title}" has been Deleted!`;
   UndoBtn.innerText = "Undo";
 
   Bar.appendChild(Message);
   Bar.appendChild(UndoBtn);
 
   UndoBtn.addEventListener("click", () => {
-    if (type == "todo") {
-      let deletedTodo = item;
-      let todos = addTodo(deletedTodo);
+    if (!undoClicked) {
+      if (type == "todo") {
+        let deletedTodo = item;
+        let todos = addTodo(deletedTodo);
 
-      if (Nav.todayBtn.className == "active-button") {
-        todos = getTodaysTodos();
-      } else {
-        todos = getListTodos(todos, deletedTodo.list);
+        if (Nav.todayBtn.className == "active-button") {
+          todos = getTodaysTodos();
+        } else {
+          todos = getListTodos(todos, deletedTodo.list);
+        }
+
+        clearContent();
+        updateContent(todos, "todo");
+      } else if (type == "list") {
+        let lists = addList(item);
+
+        clearContent();
+        updateContent(lists, "list");
       }
-
-      clearContent();
-      updateContent(todos, "todo");
-    } else if (type == "list") {
-      let lists = addList(item);
-
-      clearContent();
-      updateContent(lists, "list");
+      undoClicked = true;
     }
   });
 
